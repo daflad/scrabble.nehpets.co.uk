5c4e0511628553a66651279d0d1b3ed830d66aec
Stephen John Russell <s.russell@thedataexchange.com>
2020-05-04 13:30:41 +0100

    removed demo pieces, updataed css started to comment controller

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -5,15 +5,13 @@ import (
 	"github.com/revel/revel"
 )
 
+// App controller
 type App struct {
 	*revel.Controller
 }
 
+// Index page for now
 func (c App) Index() revel.Result {
 	board := pieces.DefualtBoard()
-	board.Tiles[1][6].Value = "J"
-	board.Tiles[1][6].Points = 8
-	board.Tiles[1][7].Value = "O"
-	board.Tiles[1][7].Points = 1
 	return c.Render(board)
 }
